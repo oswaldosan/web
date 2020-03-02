df41c0837f9fcf76ceb4778483fb0dbf1011f930
Oswaldo Antonio Sanchez <mago.ads@gmail.com>
2020-03-02 10:32:04 -0600

    update new pc
    test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> <h1>TonyLoops</h1> </p>
+        <p> <h1>TonyLoops Trest Clone</h1> </p>
           <Boton></Boton>
        </header>
     </div>
